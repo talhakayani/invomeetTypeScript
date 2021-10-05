e93c6cc7070d81354b8047b7cd802c61b411195d
talha kayani <talha.kayani@invozone.com>
2021-10-05 15:52:37 +0500

    Update

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,7 +5,7 @@ import path from 'path';
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../../config/config.json')[env];
 const db: any = {};
 
 let sequelize;
